f2bd8367f96023e0eb36e1799f159b6f87c463e6
Eternal1 <sent1@ukr.net>
2016-09-19 14:11:55 +0300

    replaced polyfills for ie

--- src/js/game/app.js
+++ src/js/game/app.js
@@ -1,4 +1,7 @@
-require('babel-polyfill');
+import 'core-js/fn/array/from'
+import 'core-js/fn/array/for-each'
+import 'core-js/es6/promise'
+import 'whatwg-fetch'
 
 import Game from './classes/Game'
 
